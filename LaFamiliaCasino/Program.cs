fde39c8855bb440c7ea4fbc877f0ff5005f8856d
Daniel Stensig <daniel@jo-informatik.dk>
2023-12-20 16:00:56 +0100

    Added main

---
+++ LaFamiliaCasino/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaFamiliaCasino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
